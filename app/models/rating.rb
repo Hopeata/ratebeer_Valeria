edc31779cde9ad0000317960dc06a937d4248cda
Hopeata <valeria.verkhovskaia@gmail.com>
2015-02-19 14:13:33 +0200

    Everything

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,7 @@ class Rating < ActiveRecord::Base
   belongs_to :beer
   belongs_to :user
 
+  scope :recent, Rating.limit(5).order("created_at DESC")
   validates :score, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 50, only_integer: true}
 
   def to_s
--- lib/rating_average.rb
+++ lib/rating_average.rb
@@ -1,10 +1,5 @@
 module RatingAverage
   def average_rating
-    rating = 0.0
-    ratings.each do |x|
-      rating = rating + x.score
-    end
-    average = rating / ratings.count
-    "Has #{ratings.count} " + "rating".pluralize(ratings.count) + ", average #{average}"
+    ratings.map(&:score).sum.to_f/ratings.count
   end
 end
